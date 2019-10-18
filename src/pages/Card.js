efb45209a9a0e3eeb0c98062e1e7f8ca1d46584f
Joshua Jones <joshuajones@trentu.ca>
2019-10-18 17:42:58 +0700

    draft changes to card

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,31 +1,40 @@
 import React from 'react'
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonIcon, IonItem, IonLabel, IonApp, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonIcon, IonItem, IonLabel, IonApp, IonHeader, IonToolbar, IonTitle, IonToggle, IonList } from '@ionic/react';
+import '../styles/card.css'
 
 
 class Card extends React.Component {
 	render() {
 		return(
-			<IonContent>
-				<IonApp>
-			    <IonHeader translucent>
-			      <IonToolbar>
-			        <IonTitle>Card</IonTitle>
-			      </IonToolbar>
-			    </IonHeader>,
-			    <IonContent fullscreen>
-			      <IonCard>
-			        <img src="https://images.unsplash.com/photo-1571253466461-fda63ff89385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" />
-			        <IonCardHeader>
-			          <IonCardSubtitle>Destination</IonCardSubtitle>
-			          <IonCardTitle>Madison, WI</IonCardTitle>
-			        </IonCardHeader>
-			        <IonCardContent>
-			          Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of the Wisconsin Territory in 1836.
-			        </IonCardContent>
-			      </IonCard>
-			    </IonContent>
-			  </IonApp>
-			</IonContent>
+			<>
+				<IonContent className="card">
+					<IonApp>
+				    <IonHeader translucent>
+				      <IonToolbar>
+				        <IonTitle>Card</IonTitle>
+				      </IonToolbar>
+				    </IonHeader>,
+				    <IonContent fullscreen>
+				      <IonCard>
+				        <img src="https://images.unsplash.com/photo-1571253466461-fda63ff89385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" />
+				        <IonCardHeader>
+				          <IonCardSubtitle>Destination</IonCardSubtitle>
+				          <IonCardTitle>Madison, WI</IonCardTitle>
+				        </IonCardHeader>
+				        <IonCardContent>
+				          Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of the Wisconsin Territory in 1836.
+				        </IonCardContent>
+								<IonItem>
+									<div>No</div>
+									<IonLabel></IonLabel>
+									<IonToggle value="yes" onChange={() => {}} />
+									<div>Yes</div>
+								</IonItem>
+				      </IonCard>
+				    </IonContent>
+				  </IonApp>
+				</IonContent>
+			</>
 		)
 	}
 }
